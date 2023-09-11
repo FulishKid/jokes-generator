64830c348be96364557dcfe6883194fbd537904d
FulishKid <pavvvelkosss@gmail.com>
2023-09-11 22:29:56 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Chuck norris jokes generetor
+
+Vanilla JavaScript Jokes Generator with some options where you can select specific category.
+Have fun!
---
+++ script.js
@@ -0,0 +1,54 @@
+const select = document.getElementById('select');
+const btn = document.getElementById('joke-btn');
+const div = document.getElementById('joke');
+const divContent = document.getElementById('joke-content');
+const xhrCategory = new XMLHttpRequest();
+const xhrJoke = new XMLHttpRequest();
+let apiURL = `https://api.chucknorris.io/jokes/random`;
+
+function createCategoryOptions() {
+  xhrCategory.open('GET', 'https://api.chucknorris.io/jokes/categories');
+  xhrCategory.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      data.forEach((category) => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.appendChild(document.createTextNode(category));
+
+        select.appendChild(option);
+      });
+    }
+  };
+  xhrCategory.send();
+}
+
+function checkForCategory() {
+  select.addEventListener('change', (e) => {
+    const category = e.target.value;
+    if (category !== 'all') {
+      apiURL = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    } else apiURL = `https://api.chucknorris.io/jokes/random`;
+  });
+}
+
+function getJoke() {
+  xhrJoke.open('GET', apiURL);
+  xhrJoke.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      divContent.innerHTML = '';
+
+      const data = JSON.parse(this.responseText);
+      const b = document.createElement('b');
+
+      b.appendChild(document.createTextNode(`"${data.value}"`));
+      divContent.style.marginBottom = '25px';
+      divContent.appendChild(b);
+    }
+  };
+  xhrJoke.send();
+}
+
+createCategoryOptions();
+checkForCategory();
+btn.addEventListener('click', getJoke);
